b2f0770740fdca60f4c97749fbcc4db9b249115e
blm-takahashi <106038848+takaki1111@users.noreply.github.com>
2022-08-21 21:11:56 +0900

    Update test_stea.py

--- test_stea.py
+++ test_stea.py
@@ -244,7 +244,9 @@ user_input = get_text()
 
 if st.session_state.count == 0:
     st.session_state.past.append("あなた")
-    st.session_state.generated.append("私の名前はブルーミーちゃん。株式会社ブルームテクノロジーのマスコットキャラクターです！")
+    st.session_state.generated.append("私の名前はブルーミーちゃん。ブルームテクノロジーのことならなんでも聞いてね！！")
+　　st.session_state.past.append("データサイエンス課の課長って誰なんだろう？")
+　　st.session_state.generated.append("「データサイエンス課の課長って誰」と聞いてみましょう！")
     st.session_state.count += 1 #値の更新
 
 else:

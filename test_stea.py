1a516b6d4b5976a5bd2222b829a20276ce52c98d
blm-takahashi <106038848+takaki1111@users.noreply.github.com>
2022-08-22 00:09:37 +0900

    Update test_stea.py

--- test_stea.py
+++ test_stea.py
@@ -319,5 +319,5 @@ else:
 if st.session_state['generated']:
 
     for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state["generated"][i], key=str(i),avatar_style="female")
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
